60334650ed4eecd5e39b57442686b47c475cebd4
pava <pavareddy1030@gmail.com>
2024-08-28 18:02:33 +0530

    file added

--- 13-colors.sh
+++ 13-colors.sh
@@ -1,37 +1,38 @@
 #!/bin/bash
 
-USERID=$(id -u)
+userid=$(id -u)
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 
 CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
+
+    if [ $userid -ne 0 ]
     then
         echo "Please run this script with root priveleges"
         exit 1
     fi
+
 }
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N"
+        echo "$2 is...$R FAILED $N"
         exit 1
     else
-        echo -e "$2 is...$G SUCCESS $N"
+        echo "$2 is...$G SUCCESS $N"
     fi
 }
 
 CHECK_ROOT
-
 dnf list installed git
 
-if [ $? -ne 0 ]
-then
+ if [ $? -ne 0 ]
+ then
     echo "Git is not installed, going to install it.."
-    dnf install git -y
-    VALIDATE $? "Installing Git"
+    dnf install git -y # here installing
+    VALIDATE $? "installing git"
 else
     echo "Git is already installed, nothing to do.."
 fi
@@ -39,10 +40,10 @@ fi
 dnf list installed mysql
 
 if [ $? -ne 0 ]
-then
-    echo "MySQL is not installed...going to install"
-    dnf install mysql -y
-    VALIDATE $? "Installing MySQL"
+ then
+    echo "mysql is not installed, going to install it.."
+    dnf install mysql -y # here installing
+    VALIDATE $? "installing mysql"
 else
-    echo "MySQL is already installed..nothing to do"
+    echo "mysql is already installed, nothing to do.."
 fi
