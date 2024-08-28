7200f6a00524b15ea1df5bc07f73247ccf46395f
pava <pavareddy1030@gmail.com>
2024-08-28 15:39:08 +0530

    file added

--- 11-installation.sh
+++ 11-installation.sh
@@ -8,3 +8,35 @@ then
     exit 0
 fi
  dnf list installed git
+
+ if [ $? -ne 0 ]
+ then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y # here installing
+    if [ $? -ne 0 ]
+    then
+        echo "Git installation is not success...check it"
+        exit 1
+    else
+        echo "Git installation is success"
+    fi
+else
+    else "Git is already installed, nothing to do.."
+fi
+
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+ then
+    echo "mysql is not installed, going to install it.."
+    dnf install mysql -y # here installing
+    if [ $? -ne 0 ]
+    then
+        echo "mysql installation is not success...check it"
+        exit 1
+    else
+        echo "mysql installation is success"
+    fi
+else
+    else "mysql is already installed, nothing to do.."
+fi
