e0a11b4377beecbe10d1f82bdcc95edc9e3518e2
pava <pavareddy1030@gmail.com>
2024-08-28 11:08:29 +0530

    file added

--- 11-installation.sh
+++ 11-installation.sh
@@ -9,3 +9,11 @@ then
 fi
 
 dnf list installed git # Just checking whether installed or not
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install git -y # here installing
+else
+    echo "Git is already installed, nothing to do.."
+fi
