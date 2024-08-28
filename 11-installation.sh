8489057e7ee68eb909649bc3966bc414a4479bfd
pava <pavareddy1030@gmail.com>
2024-08-28 12:07:35 +0530

    file added

--- 11-installation.sh
+++ 11-installation.sh
@@ -24,3 +24,29 @@ then
 else
     echo "Git is already installed, nothing to do.."
 fi
+
+dnf list installed mysql 
+
+if [ $userid -ne 0 ]
+then
+    echo "Please run this script with root priveleges"
+    exit 1
+fi
+
+dnf list installed git # Just checking whether installed or not
+
+if [ $? -ne 0 ]
+then
+    echo "Git is not installed, going to install it.."
+    dnf install mysql -y # here installing
+    if [ $? -ne 0]
+    then
+        echo "mysql installation is not success...check it"
+        exit 1
+    else
+        echo "mysql installation is success"
+    fi
+else
+    echo "mysql is already installed, nothing to do.."
+fi
+
