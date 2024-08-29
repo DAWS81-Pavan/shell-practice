1e11dfe0e8ec1865735e6757581c8a35b586da76
pava <pavareddy1030@gmail.com>
2024-08-29 15:08:39 +0530

    file added

--- 16-redirectors.sh
+++ 16-redirectors.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-
-LOGS_FOLDER="/var/log/shell-script"
+LOG_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
@@ -10,13 +9,12 @@ userid=$(id -u)
 R="\e[31m" #RED
 G="\e[32m" #GREEN
 N="\e[0m"  #NARMAL
-Y="\e[33m"
 
 CHECK_ROOT(){
 
     if [ $userid -ne 0 ]
     then
-        echo -e "$R Please run this script with root priveleges $N" | tee -a $LOG_FILE
+        echo "Please run this script with root priveleges"&>>$LOG_FILE
         exit 1
     fi
 
@@ -25,36 +23,24 @@ CHECK_ROOT(){
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$2 is...$R FAILED $N" | tee -a $LOG_FILE
+        echo -e "$2 is...$R FAILED $N" &>>$LOG_FILE
         exit 1
     else
-        echo -e "$2 is...$G SUCCESS $N" | tee -a $LOG_FILE
+        echo -e "$2 is...$G SUCCESS $N" &>>$LOG_FILE
     fi
 }
 
-USAGE(){
-    echo -e "$R USAGE:: $N sudo sh 16-redirectors.sh package1 package2 ..." | tee -a $LOG_FILE
-    exit 1
-}
-
-echo "Script started executing at: $(date)" | tee -a $LOG_FILE
-
 CHECK_ROOT
-
-if [ $# -eq 0 ]
-then
-    USAGE
-fi
 
 for package in $@
 do
     dnf list installed $package &>>$LOG_FILE
     if [ $? -ne 0 ]
     then
-        echo "$package is not installed, going to install it.." | tee -a $LOG_FILE
+        echo "$package is not installed, going to install it.." &>>$LOG_FILE
         dnf install $package -y &>>$LOG_FILE
-        VALIDATE $? "installing $package"
+        VALIDATE $? "installing $package" &>>$LOG_FILE
     else
-        echo -e "$package is already $Y installed, nothing to do..$N " | tee -a $LOG_FILE
+        echo "$package is already installed, nothing to do.." &>>$LOG_FILE
     fi
 done
