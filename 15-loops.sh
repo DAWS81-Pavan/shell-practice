10ea18b56eb0c16e4a8a9eb4dbff4ad932e289ea
pava <pavareddy1030@gmail.com>
2024-08-29 10:44:44 +0530

    file added

---
+++ 15-loops.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+userid=$(id -u)
+R="\e[31m" #RED
+G="\e[32m" #GREEN
+N="\e[0m"  #NARMAL
+
+CHECK_ROOT(){
+
+    if [ $userid -ne 0 ]
+    then
+        echo "Please run this script with root priveleges"
+        exit 1
+    fi
+
+}
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is...$R FAILED $N"
+        exit 1
+    else
+        echo -e "$2 is...$G SUCCESS $N"
+    fi
+}
+
+CHECK_ROOT
+
+for package in $@
+do
+    echo $package
+done
+
+# dnf list installed git
+
+#  if [ $? -ne 0 ]
+#  then
+#     echo "Git is not installed, going to install it.."
+#     dnf install git -y # here installing
+#     VALIDATE $? "installing git"
+# else
+#     echo "Git is already installed, nothing to do.."
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+#  then
+#     echo "mysql is not installed, going to install it.."
+#     dnf install mysql -y # here installing
+#     VALIDATE $? "installing mysql"
+# else
+#     echo "mysql is already installed, nothing to do.."
+# fi
