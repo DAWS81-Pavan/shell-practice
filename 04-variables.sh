9804515943322b6ea4268f0d8790dda4dc6bdf82
pava <pavareddy1030@gmail.com>
2024-08-31 23:07:42 +0530

    file added

--- 04-variables.sh
+++ 04-variables.sh
@@ -3,7 +3,7 @@
 person1=$1
 person2=$2
 
-echo "$1:: Hi $2, How are you?"
+echo "$1: Hi $2, How are you?"
 echo "$2:: Hello $1. I am fine. How are you doing?"
 echo "$1:: I am doing good $2. What's going on?"
 echo "$2:: I started learning Shell Script $1"
