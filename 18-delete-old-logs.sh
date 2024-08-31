7c8c7733e878b7b2647d883e5fe36ef32d2fd205
pava <pavareddy1030@gmail.com>
2024-08-31 19:09:49 +0530

    file added

--- 18-delete-old-logs.sh
+++ 18-delete-old-logs.sh
@@ -14,5 +14,5 @@ else
     echo -e "$R $SOURCE_DIR $N  not exists"
 fi
 
-FILES=$( find $SOURCE_DIR -name "*.log" -mtime +14 ls -l {}\;)
+FILES=$( find $SOURCE_DIR -name "*.log" -mtime +14 -exec ls -l {}\;)
 echo files: $FILES
