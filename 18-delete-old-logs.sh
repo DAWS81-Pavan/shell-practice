ba2d25cec72f98b9bfc593a42c788486f3adae1c
pava <pavareddy1030@gmail.com>
2024-08-31 19:01:43 +0530

    file added

---
+++ 18-delete-old-logs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SOURCE_DIR=/home/ec2-user/log
+userid=$(id -u)
+R="\e[31m" #RED
+G="\e[32m" #GREEN
+N="\e[0m"  #NARMAL
+Y="\e[33m" #YELLOW
+
+if [ -d $SOURCE_DIR ]
+then
+    echo -e "$G $SOURCE_DIR $N exists"
+else
+    echo -e "$R $SOURCE_DIR $N  not exists"
+fi
+
