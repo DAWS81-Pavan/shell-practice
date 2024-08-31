8789c1186b9591e0158c74ca534e3b580a28472d
pava <pavareddy1030@gmail.com>
2024-08-31 23:20:32 +0530

    file added

--- 06-array.sh
+++ 06-array.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # index starts from 0, size is 3
-FRUITS=("APPLE" "KIWI" "ORANGE") #Array
+# FRUITS=("APPLE" "KIWI" "ORANGE") #Array
 
-echo "First fruit is: ${FRUITS[0]}"
-echo "Second fruit is: ${FRUITS[1]}"
-echo "Third fruit is: ${FRUITS[2]}"
+# echo "First fruit is: ${FRUITS[0]}"
+# echo "Second fruit is: ${FRUITS[1]}"
+# echo "Third fruit is: ${FRUITS[2]}"
 
-echo "Third fruit is: ${FRUITS[@]}"
+# echo "Third fruit is: ${FRUITS[@]}"
+
+numbers=("1" "2" "3") #Array
+
+echo first number is ${number[0]}
+echo first number is ${number[1]}
+echo first number is ${number[2]}
