ce0beefd3fb942b8034a97a9437ca1caf99b11b3
pava <pavareddy1030@gmail.com>
2024-09-01 21:34:21 +0530

    file added

--- 19-backup.sh
+++ 19-backup.sh
@@ -35,7 +35,7 @@ FILES=$(find ${SOURCE_DIR} -name "*.log" -mtime +14)
 
 echo "Files: $FILES"
 
-if [ ! -z $FILES ] #true if FILES is empty, ! nakes it expression false
+if [ ! -z "$FILES" ] #true if FILES is empty, ! nakes it expression false
 then
     echo "Files are found"
 else
